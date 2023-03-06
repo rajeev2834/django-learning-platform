c2bf1a7d527ebcc8fbd0c7c55fc0a074dca46120
rajeev2834 <rajeev2834@gmail.com>
2023-03-06 23:41:58 +0530

    "Count subscriptions for each enrollments"

--- learning/admin.py
+++ learning/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
+from django.db.models import Count
 from . import models
 
 # Register your models here.
@@ -11,7 +12,7 @@ class StudentAdmin(admin.ModelAdmin):
     search_fields = ('user__first_name', 'user__last_name', 'user__email', 'phone_number')
     list_per_page = 10
     list_editable = ('phone_number',)
-    list_filter = ('created_at', 'updated_at')
+    list_filter = ('is_subscribed',)
     list_select_related = ('user',)
     ordering = ('user__first_name', 'user__last_name')
 
@@ -26,15 +27,25 @@ class StudentAdmin(admin.ModelAdmin):
     
 @admin.register(models.Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'subscription_type', 'price')
+    list_display = ('title', 'subscription_type', 'price', 'students_count')
     search_fields = ('title',)
+
+    def students_count(self, subscription):
+        return subscription.students_count
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(students_count= Count('enrollment'))
+    
+    students_count.short_description = 'Students Count'
 
 @admin.register(models.Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
+    exclude = ('expiration_date',)
     list_display = ('student', 'subscription', 'enrollment_date', 'expiration_date')
     list_per_page = 10
     list_filter = ('subscription',)
     ordering = ('-enrollment_date',)
+   
 
 @admin.register(models.Payment)
 class PaymentAdmin(admin.ModelAdmin):
@@ -56,11 +67,25 @@ class PassageAdmin(admin.ModelAdmin):
 class TypedPassageAdmin(admin.ModelAdmin):
     pass
 
+
+class StudentFilter(admin.SimpleListFilter):
+    title = 'student'
+    parameter_name = 'student'
+
+    def lookups(self, request, model_admin):
+        students = models.Student.objects.all()
+        return [(student.id, str(student)) for student in students]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(student_id=self.value())
+        else:
+            return queryset
 @admin.register(models.Result)
 class ResultAdmin(admin.ModelAdmin):
     list_display = ('student', 'passage', 'subexam', 'test_date')
     list_per_page = 10
-    list_filter = ('subexam',)
+    list_filter = ('subexam', StudentFilter)
     ordering = ('-test_date',)
 
 
