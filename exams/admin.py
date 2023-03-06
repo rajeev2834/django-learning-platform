b430d5be3b525120bb7fcabd2300eb8d5a57ceea
rajeev2834 <rajeev2834@gmail.com>
2023-03-07 00:08:11 +0530

    "SubEXam Admin file edited"

--- exams/admin.py
+++ exams/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 from . import models
 
 # Register your models here.
@@ -15,8 +16,14 @@ class ExamAdmin(admin.ModelAdmin):
 @admin.register(models.SubExam)
 class SubExamAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'slug')
+    list_display = ('title', 'slug', 'students_count')
     search_fields = ('title',)
     list_per_page = 10
     list_filter = ('created_at', 'updated_at')
     ordering = ('created_at',)
+
+    def students_count(self, subexam):
+        return subexam.students_count()
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(students_count = Count('result'))
